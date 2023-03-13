e968bb20ed4b76fb1dbe1a54d7a50f23b89163a9
Ritchie <flamener@hotmail.com>
2023-03-12 19:29:12 -0700

    Add ChessPiece component and assets

---
+++ src/ChessPiece.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ChessPiece.css';
+
+// Create a ChessPiece component
+// svg chess pieces from https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces
+
+
+function ChessPiece({ piece }) {
+    const pieceName = piece ? `${piece.color}-${piece.type}` : '';
+    const pieceSrc = piece ? `/assets/${pieceName}.svg` : '';
+
+    return (
+        <div className="chess-piece">
+            {piece && <img src={pieceSrc} alt={pieceName} />}
+        </div>
+    );
+}
+
+export default ChessPiece;
--- src/Chessboard.js
+++ src/Chessboard.js
@@ -5,6 +5,41 @@ const Chessboard = () => {
     // Define the initial state for the selected square
     const [selectedSquare, setSelectedSquare] = useState(null);
 
+    const [pieces, setPieces] = useState({
+        '0-0': { color: 'black', type: 'rook' },
+        '0-1': { color: 'black', type: 'knight' },
+        '0-2': { color: 'black', type: 'bishop' },
+        '0-3': { color: 'black', type: 'queen' },
+        '0-4': { color: 'black', type: 'king' },
+        '0-5': { color: 'black', type: 'bishop' },
+        '0-6': { color: 'black', type: 'knight' },
+        '0-7': { color: 'black', type: 'rook' },
+        '1-0': { color: 'black', type: 'pawn' },
+        '1-1': { color: 'black', type: 'pawn' },
+        '1-2': { color: 'black', type: 'pawn' },
+        '1-3': { color: 'black', type: 'pawn' },
+        '1-4': { color: 'black', type: 'pawn' },
+        '1-5': { color: 'black', type: 'pawn' },
+        '1-6': { color: 'black', type: 'pawn' },
+        '1-7': { color: 'black', type: 'pawn' },
+        '6-0': { color: 'white', type: 'pawn' },
+        '6-1': { color: 'white', type: 'pawn' },
+        '6-2': { color: 'white', type: 'pawn' },
+        '6-3': { color: 'white', type: 'pawn' },
+        '6-4': { color: 'white', type: 'pawn' },
+        '6-5': { color: 'white', type: 'pawn' },
+        '6-6': { color: 'white', type: 'pawn' },
+        '6-7': { color: 'white', type: 'pawn' },
+        '7-0': { color: 'white', type: 'rook' },
+        '7-1': { color: 'white', type: 'knight' },
+        '7-2': { color: 'white', type: 'bishop' },
+        '7-3': { color: 'white', type: 'queen' },
+        '7-4': { color: 'white', type: 'king' },
+        '7-5': { color: 'white', type: 'bishop' },
+        '7-6': { color: 'white', type: 'knight' },
+        '7-7': { color: 'white', type: 'rook' },
+    });
+
     // Define a helper function to determine if a square is black or white
     const isBlackSquare = (x, y) => {
         return (x + y) % 2 === 1;
@@ -34,11 +69,23 @@ const Chessboard = () => {
                                 } ${selectedSquare && selectedSquare.x === x && selectedSquare.y === y
                                     ? 'chessboard-square--selected'
                                     : ''
-                                }`}
+                                }
+                                ${selectedSquare && selectedSquare.x === x && selectedSquare.y === y && !pieces[`${x}-${y}`] ? 'chessboard-square--empty' : ''}
+                                    `}
                             onClick={() => handleSquareClick(x, y)}
                         >
                             {/* Render the piece here */}
-                            {/* If the square is selected, render the piece */}
+                            {pieces[`${x}-${y}`] && (
+                                <span className={`chess-piece chess-piece--${pieces[`${x}-${y}`].color}`}>
+                                    {pieces[`${x}-${y}`].type === 'pawn' && '♙'}
+                                    {pieces[`${x}-${y}`].type === 'rook' && '♖'}
+                                    {pieces[`${x}-${y}`].type === 'knight' && '♘'}
+                                    {pieces[`${x}-${y}`].type === 'bishop' && '♗'}
+                                    {pieces[`${x}-${y}`].type === 'queen' && '♕'}
+                                    {pieces[`${x}-${y}`].type === 'king' && '♔'}
+                                </span>
+                            )}
+
 
                         </div>
                     ))}
